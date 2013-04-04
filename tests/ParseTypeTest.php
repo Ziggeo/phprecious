142140c1cbe78ba6003d16e929760e33a024cb1a
Oliver Friedmann <oliverfriedmann@Oliver-Friedmanns-MacBook-Air.local>
2013-04-04 11:47:47 -0400

    Added Test

---
+++ tests/ParseTypeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(dirname(__FILE__) . "/../support/strings/ParseType.php");
+
+class ParseTypeTest extends PHPUnit_Framework_TestCase {
+	
+	public function testBool() {
+		$this->assertTrue(ParseType::parseBool(1));
+		$this->assertTrue(ParseType::parseBool(TRUE));
+		$this->assertTrue(ParseType::parseBool("on"));
+		$this->assertTrue(ParseType::parseBool("yes"));
+		$this->assertTrue(ParseType::parseBool("true"));
+		$this->assertFalse(ParseType::parseBool(0));
+		$this->assertFalse(ParseType::parseBool(FALSE));
+		$this->assertFalse(ParseType::parseBool("off"));
+		$this->assertFalse(ParseType::parseBool("no"));
+		$this->assertFalse(ParseType::parseBool("false"));
+		$this->assertFalse(ParseType::parseBool(NULL));
+	}
+	
+}
+
