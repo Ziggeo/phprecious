5ffe8dba139d1c5faeb9b24516d0e4d32544162b
Oliver Friedmann <oliver.friedmann@gmail.com>
2018-06-05 11:43:03 +0200

    Slack

--- logging/LoggerSlackConsumer.php
+++ logging/LoggerSlackConsumer.php
@@ -4,22 +4,32 @@
 require_once(dirname(__FILE__) . "/LoggerConsumer.php");
 
 class LoggerSlackConsumer extends LoggerConsumer {
-	
+
 	private $slackChannel;
 	private $slackUsername;
 	private $slackWebhookUri;
-	
+
 	public function __construct($slackWebhook, $slackChannel, $slackUsername) {
 		$this->slackChannel = $slackChannel;
 		$this->slackUsername = $slackUsername;
 		$this->slackWebhookUri = $slackWebhook;
 	}
-		
+
+	private function preprocessMessage($s) {
+        return preg_replace_callback(
+            '/[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/',
+            function ($matches) {
+                return "<mailto:" . $matches[0] . "|" . $matches[0] . ">";
+            },
+            $s
+        );
+    }
+
 	protected function processMessage($logger, $logMessage) {
 		$ch = curl_init($this->slackWebhookUri);
 		$data = json_encode([
 			"channel" => APP()->config("slack.channel"),
-			"text" => $logMessage->formatOneLine(),
+			"text" => $this->preprocessMessage($logMessage->formatOneLine()),
 			"username" => APP()->config("slack.username"),
 		]);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
