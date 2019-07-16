87d8724fcda327129f697666cc8d9cf96246c282
Pablo Iglesias <pablo.iglesias02@gmail.com>
2019-07-16 14:42:42 -0300

    Changed S3 file system config

--- files/S3FileSystem.php
+++ files/S3FileSystem.php
@@ -5,135 +5,140 @@ require_once(dirname(__FILE__) . "/AbstractFileSystem.php");
 
 
 Class S3FileSystem extends AbstractFileSystem {
-	
+
 	private $s3;
 	private $bucket;
-	
-	protected function getClass() {
-		return S3File;
-	}
-	
-	function __construct($key, $secret, $bucket, $region = "us-east-1", $signature = "") {
-		// parent::__construct();
-		try {
-			$credentials = new Aws\Credentials\Credentials($key, $secret);
-			$conf = array(
-				"credentials" => $credentials,
-				"region" => $region,
-				"profile" => "default",
-				"version" => "2006-03-01"
 
+	protected function getClass() {
+		return "S3File";
+	}
+
+	function __construct($opts) {
+		try {
+			$conf = array(
+				"region" => $opts["region"],
+				"version" => "2006-03-01"
 			);
-			if ($signature !== "v2")
-				$conf["signature"] = $signature;
+			//Using credentials when set in the config.
+			if ($opts["key"] <> "" && $opts["secret"] <> "")
+				$conf["credentials"] = new Aws\Credentials\Credentials($opts["key"], $opts["secret"]);
+			//Allowing the use of profiles.
+			if (($opts["key"] === "" || $opts["secret"] === "") && $opts["profile"] <> "")
+				$conf["profile"] = $opts["profile"];
+			if ($opts["signature"] !== "v2")
+				$conf["signature"] = $opts["signature"];
 			$this->s3 = new Aws\S3\S3Client($conf);
-			$this->bucket = $bucket;
+			$this->bucket = $opts["bucket"];
 		} catch (Exception $e) {
 			throw new FileSystemException($e->getMessage());
 		}
 	}
-	
+
 	public function s3() {
 		return $this->s3;
 	}
-	
+
 	public function bucket() {
 		return $this->bucket;
 	}
-	
+
 }
 
 
 Class S3File extends AbstractFile {
-	
+
+	public function filename() {
+		return ltrim($this->file_name, "/"); //For the new version the string shouldn"t start with "/"
+	}
+
 	private function s3() {
 		return $this->file_system->s3();
 	}
-	
+
 	private function bucket() {
 		return $this->file_system->bucket();
 	}
-	
+
 	public function s3path() {
-		return 's3://' . $this->bucket() . '/' . $this->file_name;
+		return "s3://" . $this->bucket() . ""/"" . $this->filename();
 	}
-	
+
 	public function waitUntilExists($options = array("wait_time" => 1000, "repeat_count" => 3)) {
-		$this->s3()->waitUntil('ObjectExists', array(
-			'Bucket' => $this->bucket(),
-			'Key' => $this->file_name,
-			'waiter.interval' => ceil($options["wait_time"] / 1000),
-			'waiter.max_attempts' => $options["repeat_count"]
+		$this->s3()->waitUntil("ObjectExists", array(
+			"Bucket" => $this->bucket(),
+			"Key" => $this->filename(),
+			"waiter.interval" => ceil($options["wait_time"] / 1000),
+			"waiter.max_attempts" => $options["repeat_count"]
 		));
 	}
-	
+
 	public function size() {
 		$meta = $this->s3()->headObject(array(
 			"Bucket" => $this->bucket(),
-			"Key" => $this->file_name
+			"Key" => $this->filename()
 		));
 		return intval($meta["ContentLength"]);
 	}
-	
+
 	public function exists() {
 		try {
 			$meta = $this->s3()->headObject(array(
 				"Bucket" => $this->bucket(),
-				"Key" => $this->file_name
+				"Key" => $this->filename()
 			));
 			return !!@$meta;
 		} catch (Exception $e) {
 			return FALSE;
 		}
 	}
-	
+
 	public function delete() {
 		try {
 			$meta = $this->s3()->deleteObject(array(
 				"Bucket" => $this->bucket(),
-				"Key" => $this->file_name
+				"Key" => $this->filename()
 			));
 		} catch (Exception $e) {
 			throw new FileSystemException("Could not delete file");
 		}
 	}
-	
+
 	public function readStream() {
 		$handle = fopen($this->s3path(), "r");
 		if ($handle === FALSE)
 			throw new FileSystemException("Could not open file");
 		return $handle;
 	}
-	
+
 	public function writeStream() {
 		$handle = fopen($this->s3path(), "w");
 		if ($handle === FALSE)
 			throw new FileSystemException("Could not open file");
 		return $handle;
 	}
-			
+
 	public function toLocalFile($file) {
 		try {
 			$this->s3()->getObject(array(
-				'Bucket' => $this->bucket(),
-				'Key'    => $this->file_name,
-				'SaveAs' => $file
+				"Bucket" => $this->bucket(),
+				"Key" => $this->filename(),
+				"SaveAs" => $file
 			));
 		} catch (Exception $e) {
 			throw new FileSystemException($e->getMessage());
 		}
 	}
-	
+
 	public function fromLocalFile($file) {
 		try {
-			 $this->s3()->putObject(array(
-				'Bucket' => $this->bucket(),
-				'Key'    => $this->file_name,
-				'SourceFile' => $file
+			$this->s3()->putObject(array(
+				"Bucket" => $this->bucket(),
+				"Key" => $this->filename(),
+				"SourceFile" => $file
 			));
 		} catch (Exception $e) {
 			throw new FileSystemException($e->getMessage());
 		}
-	}	
+	}
 
 }
