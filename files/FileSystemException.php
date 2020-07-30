24a985dc283a2f805bb876b4615e991ae6272575
Pablo Iglesias <pablo.iglesias02@gmail.com>
2020-07-30 18:44:27 -0300

    Fix for S3 Region problem

--- files/FileSystemException.php
+++ files/FileSystemException.php
@@ -2,3 +2,14 @@
 
 Class FileSystemException extends Exception {}
 
+Class FileSystemFieldException extends FileSystemException {
+	private $data;
+
+	function __construct($data = array()) {
+		$this->data = $data;
+	}
+
+	function getData() {
+		return $this->data;
+	}
+}
