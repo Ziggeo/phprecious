37fe559e30c04b793f32337564cfc5587fa019b0
ziggeo-commits <ziggeo-commits@ziggeo.com>
2021-06-20 20:30:30 -0400

    Adjust DynamoDB Credentials

--- database/dynamodb/DynamoDBDatabase.php
+++ database/dynamodb/DynamoDBDatabase.php
@@ -10,101 +10,99 @@ use Aws\DynamoDb\Marshaler;
 
 class DynamoDBDatabase extends Database {
 
-	const TYPES = array(
-		"S" => "string",
-		"N" => "number",
-		"M" => "array",
-		"L" => "list",
-		"BOOL" => "boolean"
-	);
-	protected static function perfmon($enter) {
-		global $PERFMON;
-		if (@$PERFMON) {
-			if ($enter)
-				$PERFMON->enter("database");
-			else
-				$PERFMON->leave("database");
-		}
-	}
+    const TYPES = array(
+        "S" => "string",
+        "N" => "number",
+        "M" => "array",
+        "L" => "list",
+        "BOOL" => "boolean"
+    );
+    protected static function perfmon($enter) {
+        global $PERFMON;
+        if (@$PERFMON) {
+            if ($enter)
+                $PERFMON->enter("database");
+            else
+                $PERFMON->leave("database");
+        }
+    }
 
-	private $database;
-	private $config;
+    private $database;
+    private $config;
 
-	public function __construct($config = array()) {
-		$this->config = $config;
-	}
+    public function __construct($config = array()) {
+        $this->config = $config;
+    }
 
-	public function getDatabase() {
-		if (!$this->database) {
-			static::perfmon(TRUE);
-			$config = $this->config;
-			if (empty($config["endpoint"]))
-				throw new InvalidArgumentException("Endpoint param is mandatory");
-			if (empty($config["region"]))
-				throw new InvalidArgumentException("Region param is mandatory");
-			if (empty($config["version"]))
-				throw new InvalidArgumentException("Region param is mandatory");
-			$sdk = new Aws\Sdk($this->config);
-			$this->database = $sdk->createDynamoDb();
-			static::perfmon(FALSE);
-		}
-		return $this->database;
-	}
+    public function getDatabase() {
+        if (!$this->database) {
+            static::perfmon(TRUE);
+            $config = $this->config;
+            if (empty($config["region"]))
+                $config["region"] = "us-east-1";
+            if (empty($config["version"]))
+                $config["version"] = "latest";
+            $sdk = new Aws\Sdk($config);
+            $this->database = $sdk->createDynamoDb();
+            static::perfmon(FALSE);
+        }
+        return $this->database;
+    }
 
-	public function selectTable($name, $config = array()) {
-		return new DynamoDBDatabaseTable($this, $name, $config);
-	}
+    public function selectTable($name, $config = array()) {
+        return new DynamoDBDatabaseTable($this, $name, $config);
+    }
 
-	public function encode($type, $value, $attrs = array()) {
-		if ($type === "date")
-			return date(DATE_ISO8601, $value);
-		return $value;
-	}
-	public function decode($type, $value, $attrs = array()) {
-		$marshaler = new Marshaler();
-		if (!is_array($value)) {
-			if (($type === "string" || $type === "date" || $type === "id"))
-				$type = "S";
-			elseif ($type === "boolean")
-				$type = "BOOL";
-			return $marshaler->unmarshalValue(array($type => $value));
-		} else
-			return $marshaler->unmarshalValue($value);
-	}
+    public function encode($type, $value, $attrs = array()) {
+        if ($type === "date")
+            return date(DATE_ISO8601, $value);
+        return $value;
+    }
+    public function decode($type, $value, $attrs = array()) {
+        $marshaler = new Marshaler();
+        if (!is_array($value)) {
+            if (($type === "string" || $type === "date" || $type === "id"))
+                $type = "S";
+            elseif ($type === "boolean")
+                $type = "BOOL";
+            return $marshaler->unmarshalValue(array($type => $value));
+        } else
+            return $marshaler->unmarshalValue($value);
+    }
 
 
-	public function createTable($name, $config) {
-		if (!@$config["KeySchema"])
-			throw new InvalidArgumentException("KeyScheme attribute is mandatory");
-		if (!@$config["AttributeDefinitions"])
-			throw new InvalidArgumentException("AttributeDefinitions attribute is mandatory");
-		/*if (!@$config["ProvisionedThroughput"])
-			throw new InvalidArgumentException("ProvisionedThroughput attribute is mandatory");*/
-		$config["TableName"] = $name;
-		$this->getDatabase()->createTable($config);
-		return $this->selectTable($name, $config);
-	}
+    public function createTable($name, $config) {
+        if (!@$config["KeySchema"])
+            throw new InvalidArgumentException("KeyScheme attribute is mandatory");
+        if (!@$config["AttributeDefinitions"])
+            throw new InvalidArgumentException("AttributeDefinitions attribute is mandatory");
+        /*if (!@$config["ProvisionedThroughput"])
+            throw new InvalidArgumentException("ProvisionedThroughput attribute is mandatory");*/
+        $config["TableName"] = $name;
+        $this->getDatabase()->createTable($config);
+        return $this->selectTable($name, $config);
+    }
 
-	public function decodeItem($item) {
-		$decoded = array();
-		foreach ($item as $id_i => $value) {
-			$decoded[$id_i] = $this->decode(ArrayUtils::arrayKeyFirst($value), ArrayUtils::firstElement($value));
-		}
+    public function decodeItem($item) {
+        $decoded = array();
+        foreach ($item as $id_i => $value) {
+            $decoded[$id_i] = $this->decode(ArrayUtils::arrayKeyFirst($value), ArrayUtils::firstElement($value));
+        }
 
-		return $decoded;
-	}
+        return $decoded;
+    }
 
-	public function deleteTable($tablename) {
-		$params = [
-			'TableName' => $tablename
-		];
+    public function deleteTable($tablename) {
+        $params = [
+            'TableName' => $tablename
+        ];
 
-		try {
-			return $this->getDatabase()->deleteTable($params);
-		} catch (DynamoDbException $e) {
-			echo "Unable to delete table:\n";
-			echo $e->getMessage() . "\n";
-		}
-	}
+        try {
+            return $this->getDatabase()->deleteTable($params);
+        } catch (DynamoDbException $e) {
+            echo "Unable to delete table:\n";
+            echo $e->getMessage() . "\n";
+        }
+    }
 }
 
